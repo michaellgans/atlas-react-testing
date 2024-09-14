cc6f56c2c74777d4758fa620fde39813d14ffade
michaellgans <michaellgans@Victoria>
2024-09-14 07:53:12 -0500

    MLG - 7:52 AM - updates config files for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import react from "@vitejs/plugin-react";
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom'
+  },
 });
