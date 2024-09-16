007cd9083da279cbebf7534d62c2d54d7aafdc21
michaellgans <michaellgans@Victoria>
2024-09-15 22:28:12 -0500

    MLG - 10:27 PM - passes third test for MusicPlayer

--- src/__tests__/MusicPlayer.test.tsx
+++ src/__tests__/MusicPlayer.test.tsx
@@ -5,8 +5,8 @@ import { render } from "@testing-library/react";
 import MusicPlayer from "@/MusicPlayer";
 import { expect, test } from "vitest";
 
-test("MusicPlayer renders children correctly", () => {
-    // Generate mock children
+test("MusicPlayer renders a child correctly", () => {
+    // Generate mock child
     const { getByText } = render(
         <MusicPlayer>
             <div>
@@ -31,3 +31,19 @@ test("MusicPlayer renders correctly and matches snapshot", () => {
     // Take Snapshot
     expect(asFragment()).toMatchSnapshot();
 })
+
+test("MusicPlayer renders multiple children correctly", () => {
+    // Generate multiple children
+    const {getAllByText} = render(
+        <MusicPlayer>
+            <div>Mock Child 1</div>
+            <div>Mock Child 2</div>
+            <div>Mock Child 3</div>
+        </MusicPlayer>
+    );
+
+    // If all three children are able to render
+    // regex will find 3 matches
+    const children = getAllByText(/Mock Child \d/);
+    expect(children).toHaveLength(3);
+});
